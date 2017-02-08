417672c8e126b90dae29e3faa4eef187dd715cd9
達哉天谷 <amagai.tohoku@gmail.com>
2017-02-08 22:29:35 +0900

    署名

--- iBeaconTransmitter.Droid/Properties/AssemblyInfo.cs
+++ iBeaconTransmitter.Droid/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using Android.App;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("(c) Tatsuya Amagai")]
+[assembly: AssemblyCopyright("(c) microwavePC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
