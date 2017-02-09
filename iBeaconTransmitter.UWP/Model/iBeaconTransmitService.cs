485678a789310455cf58e6235fe942615120f179
microwavePC <amagai.tohoku@gmail.com>
2017-02-10 00:51:53 +0900

    UWP版のベースを追加

---
+++ iBeaconTransmitter.UWP/Model/iBeaconTransmitService.cs
@@ -0,0 +1,30 @@
+﻿using iBeaconTransmitter.Model;
+using iBeaconTransmitter.UWP.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(iBeaconTransmitService))]
+namespace iBeaconTransmitter.UWP.Model
+{
+    public class iBeaconTransmitService : IiBeaconTransmitService
+    {
+        public void StartTransmission(iBeacon ibeacon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartTransmission(Guid uuid, ushort major, ushort minor, sbyte txPower)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopTransmission()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
